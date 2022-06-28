fb31a75c1190134f1e30caafc4f72066349dfda1
silvermarkg <19527097+silvermarkg@users.noreply.github.com>
2022-06-28 17:41:45 +0100

    AzureAD ObjectId to SID conversion functions

---
+++ Convert-AzureADObjectIdToSid.ps1
@@ -0,0 +1,25 @@
+function Convert-AzureAdObjectIdToSid {
+<#
+.SYNOPSIS
+Convert an Azure AD Object ID to SID
+From Oliver Kieselbach (https://oliverkieselbach.com/2020/05/13/powershell-helpers-to-convert-azure-ad-object-ids-and-sids/)
+  
+.DESCRIPTION
+Converts an Azure AD Object ID to a SID.
+Author: Oliver Kieselbach (oliverkieselbach.com)
+The script is provided "AS IS" with no warranties.
+ 
+.PARAMETER ObjectID
+The Object ID to convert
+#>
+
+    param([String] $ObjectId)
+
+    $bytes = [Guid]::Parse($ObjectId).ToByteArray()
+    $array = New-Object 'UInt32[]' 4
+
+    [Buffer]::BlockCopy($bytes, 0, $array, 0, 16)
+    $sid = "S-1-12-1-$array".Replace(' ', '-')
+
+    return $sid
+}
---
+++ Convert-AzureADSidtoObjectId.ps1
@@ -0,0 +1,26 @@
+function Convert-AzureAdSidToObjectId {
+<#
+.SYNOPSIS
+Convert a Azure AD SID to Object ID
+From Oliver Kieselbach (https://oliverkieselbach.com/2020/05/13/powershell-helpers-to-convert-azure-ad-object-ids-and-sids/)
+ 
+.DESCRIPTION
+Converts an Azure AD SID to Object ID.
+Author: Oliver Kieselbach (oliverkieselbach.com)
+The script is provided "AS IS" with no warranties.
+ 
+.PARAMETER ObjectID
+The SID to convert
+#>
+
+    param([String] $Sid)
+
+    $text = $sid.Replace('S-1-12-1-', '')
+    $array = [UInt32[]]$text.Split('-')
+
+    $bytes = New-Object 'Byte[]' 16
+    [Buffer]::BlockCopy($array, 0, $bytes, 0, 16)
+    [Guid]$guid = $bytes
+
+    return $guid
+}
